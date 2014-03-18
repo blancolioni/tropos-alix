27f9dcd89c6d5b5e029372c9985bc99f8f8ef6ae
Fraser Wilson <fwilson@anago.nl>
2014-03-18 16:40:06 +0100

    Added XML writer

--- src/tropos-writer.ads
+++ src/tropos-writer.ads
@@ -3,4 +3,7 @@ package Tropos.Writer is
    procedure Write_Config (Config : Configuration;
                            Path   : String);
 
+   procedure Write_XML_Config (Config : Configuration;
+                               Path   : String);
+
 end Tropos.Writer;
